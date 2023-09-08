8ea45b6bfa9a0e5edfa1eba255f85e34e97f0f20
Sunil Mourya <sunilmourya.cse.nitp@gmail.com>
2023-09-08 20:42:42 +0530

    Ch~10: Using Fragment Arguments

--- 3E/CriminalIntent/app/src/main/java/dev/sunilmourya/criminalintent/CrimeActivity.java
+++ 3E/CriminalIntent/app/src/main/java/dev/sunilmourya/criminalintent/CrimeActivity.java
@@ -1,16 +1,26 @@
 package dev.sunilmourya.criminalintent;
 
-import android.os.Bundle;
+import android.content.Context;
+import android.content.Intent;
 
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
+
+import java.util.UUID;
 
 public class CrimeActivity extends SingleFragmentActivity {
 
+    public static final String EXTRA_CRIME_ID = "dev.sunilmourya.criminalintent.crime_id";
+
+    public static Intent newIntent(Context packageContext, UUID crimeId) {
+        Intent intent = new Intent(packageContext, CrimeActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeId);
+        return intent;
+    }
+
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        UUID crimeId = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
+        return CrimeFragment.newInstance(crimeId);
     }
 
 }
